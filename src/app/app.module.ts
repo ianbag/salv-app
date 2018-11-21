a0a8b520b20f62a6a6e2f2a719b8c90c9bc3e058
hppod <hppodgornik@gmail.com>
2018-11-21 15:58:17 -0200

    organizando a estrutura da aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './fixed-elements/header/header.component';
+import { SidebarComponent } from './fixed-elements/sidebar/sidebar.component';
+import { FooterComponent } from './fixed-elements/footer/footer.component';
 
 @NgModule({
   declarations: [
