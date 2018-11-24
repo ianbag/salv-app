736a50ba094f1e5126c50aed94e146bb7d86adc3
hppod <hppodgornik@gmail.com>
2018-11-24 00:52:41 -0200

    injetando a propriedade residente no componente

--- src/app/residentes/residente/residente.component.ts
+++ src/app/residentes/residente/residente.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
+import { ResidenteDetalhes } from './residente-detalhes.model';
 @Component({
   selector: 'salv-residente',
   templateUrl: './residente.component.html'
 })
 export class ResidenteComponent implements OnInit {
+
+  @Input() residente: ResidenteDetalhes
 
   constructor() { }
 
