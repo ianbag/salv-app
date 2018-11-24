12fe6da195247e9bb4deb112e175a6769748cc50
hppod <hppodgornik@gmail.com>
2018-11-24 16:05:05 -0200

    model residente

---
+++ src/app/residentes/residente/residente.model.ts
@@ -0,0 +1,39 @@
+export interface Residente {
+    nome: string,
+    sobrenome: string,
+    rg: string,
+    cpf: string,
+    sexo: string,
+    estado_civil: string
+    data_nascimento: string
+    religiao: string,
+    escolaridade: string,
+
+    apelido: string,
+    profissao: string,
+
+    titulo_eleitor: string,
+    zona_eleitoral: string,
+    secao_eleitoral: string,
+
+    numero_certidao_nascimento: string,
+    folha_certidao_nascimento: string,
+    livro_certidao_nascimento: string,
+    cidade_certidao_nascimento: string,
+    estado_certidao_nascimento: string,
+
+    cartao_sams: string,
+    cartao_sus: string,
+
+    numero_inss: string,
+    banco_inss: string,
+    agencia_inss: string,
+    conta_inss: string,
+    valor_inss: string,
+    situacao_inss: string,
+    prova_vida_inss: string,
+
+    data_acolhimento: string,
+    data_desacolhimento: string,
+    motivo_desacolhimento: string
+}
