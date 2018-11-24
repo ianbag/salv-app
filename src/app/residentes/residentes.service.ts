2a69280a903da82de28e4ce3b9f4e7890cd20086
hppod <hppodgornik@gmail.com>
2018-11-24 01:00:32 -0200

    implementando o metodo

--- src/app/residentes/residentes.service.ts
+++ src/app/residentes/residentes.service.ts
@@ -1,13 +1,17 @@
+import { SALV_API } from './../app.api';
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import {Observable} from "rxjs"
+import { Observable } from "rxjs"
+import { ResidenteDetalhes } from "./residente/residente-detalhes.model";
 
 @Injectable()
-export class ResidentesService{
+export class ResidentesService {
 
-    constructor(private http: HttpClient){
+    constructor(private http: HttpClient) {
 
     }
 
-    
+    residentes(): Observable<ResidenteDetalhes[]> {
+        return this.http.get<ResidenteDetalhes[]>(`${SALV_API}/pessoa`)
+    }
 }
