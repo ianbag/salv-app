7028ea2235f7129b97e1f0b38b61c0f9b255b8d5
Juliano <juliano-gema@hotmail.com>
2018-11-25 18:16:25 -0200

    mudanças

---
+++ src/app/tela-inicial/tela-inicial.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'salv-tela-inicial',
+  templateUrl: './tela-inicial.component.html',
+  styleUrls: ['./tela-inicial.component.css']
+})
+export class TelaInicialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
