4afa0856627de5c788bc83dded167517c5eb2a33
JulianoAnselmo <43391579+JulianoAnselmo@users.noreply.github.com>
2018-11-25 20:46:09 -0200

    tela inicial (#6)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { Routes } from "@angular/router";
 
 import { ResidentesComponent } from "./residentes/residentes.component";
 import { ResidenteComponent } from "./residentes/residente/residente.component";
+import { TelaInicialComponent } from './tela-inicial/tela-inicial.component';
 
 export const ROUTES: Routes = [
     //ROTAS VÃO AQUI
 
     { path: 'residentes', component: ResidentesComponent },
     { path: 'residente/:id', component: ResidenteComponent },
-    { path: 'convenios', component: ConveniosComponent }
+    { path: 'convenios', component: ConveniosComponent },
+    { path: 'tela-inicial', component: TelaInicialComponent}
 
 ]
---
+++ src/app/tela-inicial/aniversariantes/aniversariantes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'salv-aniversariantes',
+  templateUrl: './aniversariantes.component.html',
+  styleUrls: ['./aniversariantes.component.css']
+})
+export class AniversariantesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
