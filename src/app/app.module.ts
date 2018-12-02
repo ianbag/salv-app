8ed3e23efea953c62f4ac031b035bf4fc32dac72
hppod <hppodgornik@gmail.com>
2018-12-02 17:15:49 -0200

    ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,20 +24,12 @@ import { CpfPipe } from './shared/pipes/cpf.pipe';
 import { RgPipe } from './shared/pipes/rg.pipe';
 import { InfosFamiliarComponent } from './residentes/residente/infos-familiar/infos-familiar.component';
 import { ConveniosComponent } from './convenios/convenios.component';
-<<<<<<< HEAD
 import { ConveniosService } from './convenios/convenios.service';
-=======
->>>>>>> 1b6cc3466863356b6cf019954b61c51efea14101
 import { AniversariantesComponent } from './tela-inicial/aniversariantes/aniversariantes.component';
 import { ProvaDeVidaComponent } from './tela-inicial/prova-de-vida/prova-de-vida.component';
 import { AniversarianteComponent } from './tela-inicial/aniversariantes/aniversariante/aniversariante.component';
-<<<<<<< HEAD
-=======
-import { DialogConfirmService } from './residentes/dialog-confirm.service';
->>>>>>> upstream/master
 import { InfosConvenioComponent } from './residentes/residente/infos-convenio/infos-convenio.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-<<<<<<< HEAD
 import { FuncionariosComponent } from './funcionarios/funcionarios.component';
 import { FuncionarioComponent } from './funcionarios/funcionario/funcionario.component';
 import { InfosDependenteComponent } from './funcionarios/funcionario/infos-dependente/infos-dependente.component';
@@ -45,16 +37,12 @@ import { InfosFuncionaisComponent } from './funcionarios/funcionario/infos-funci
 import { NovoFuncionarioComponent } from './funcionarios/funcionario/novo-funcionario/novo-funcionario.component';
 import { NovoDependenteComponent } from './funcionarios/funcionario/novo-funcionario/novo-dependente/novo-dependente.component';
 import { NovaInfoFuncionalComponent } from './funcionarios/funcionario/novo-funcionario/nova-info-funcional/nova-info-funcional.component';
-<<<<<<< HEAD
 import { ConvenioComponent } from './convenios/convenio/convenio.component';
-=======
->>>>>>> 1b6cc3466863356b6cf019954b61c51efea14101
-=======
+import { DialogConfirmService } from './residentes/dialog-confirm.service';
 import { AcompanhamentosComponent } from './acompanhamentos/acompanhamentos.component';
 import { AcompanhamentoComponent } from './acompanhamentos/acompanhamento/acompanhamento.component';
 import { AcompanhamentosService } from './acompanhamentos/acompanhamentos.service';
 import { NovoResidenteComponent } from './residentes/novo-residente/novo-residente.component';
->>>>>>> upstream/master
 
 @NgModule({
   declarations: [
@@ -76,11 +64,6 @@ import { NovoResidenteComponent } from './residentes/novo-residente/novo-residen
     ConveniosComponent,
     AniversariantesComponent,
     ProvaDeVidaComponent,
-<<<<<<< HEAD
-    AniversarianteComponent,
-<<<<<<< HEAD
-=======
->>>>>>> upstream/master
     AniversarianteComponent,
     InfosConvenioComponent,
     NotFoundComponent,
@@ -91,22 +74,16 @@ import { NovoResidenteComponent } from './residentes/novo-residente/novo-residen
     NovoFuncionarioComponent,
     NovoDependenteComponent,
     NovaInfoFuncionalComponent,
-<<<<<<< HEAD
     InfosConvenioComponent,
     NotFoundComponent,
-    ConvenioComponent
-=======
+    ConvenioComponent,
     InfosConvenioComponent,
-    NotFoundComponent
->>>>>>> 1b6cc3466863356b6cf019954b61c51efea14101
-=======
     NotFoundComponent,
     AcompanhamentosComponent,
     InfosConvenioComponent,
     NotFoundComponent,
     AcompanhamentoComponent,
     NovoResidenteComponent
->>>>>>> upstream/master
   ],
   imports: [
     BrowserModule,
@@ -117,13 +94,10 @@ import { NovoResidenteComponent } from './residentes/novo-residente/novo-residen
     RouterModule.forRoot(ROUTES)
   ],
   providers: [
-<<<<<<< HEAD
-    ResidentesService,ConveniosService
-=======
     ResidentesService,
+    ConveniosService,
     DialogConfirmService,
     AcompanhamentosService
->>>>>>> upstream/master
   ],
   bootstrap: [AppComponent]
 })
