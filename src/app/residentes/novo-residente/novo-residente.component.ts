4d88e24bdfd795209fff25455f072c04c501813a
hppod <hppodgornik@gmail.com>
2018-12-02 15:41:16 -0200

    ajustes no template e ngfor para estados

--- src/app/residentes/novo-residente/novo-residente.component.ts
+++ src/app/residentes/novo-residente/novo-residente.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NovoResidenteComponent implements OnInit {
 
+  estados = [
+    "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"
+  ]
+
   constructor() { }
 
   ngOnInit() {
