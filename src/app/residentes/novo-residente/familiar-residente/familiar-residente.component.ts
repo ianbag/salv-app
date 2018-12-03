1a33d9422f4c050b693f444cc51e8dda3ec081e4
hppod <hppodgornik@gmail.com>
2018-12-02 22:41:33 -0200

    mascaras familiar

--- src/app/residentes/novo-residente/familiar-residente/familiar-residente.component.ts
+++ src/app/residentes/novo-residente/familiar-residente/familiar-residente.component.ts
@@ -1,3 +1,5 @@
+import { Familiar } from './../../residente/infos-familiar/familiar.model';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FamiliarResidenteComponent implements OnInit {
 
-  constructor() { }
+  estados = [
+    "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"
+  ];
+
+  familiarResidenteForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.familiarResidenteForm = this.formBuilder.group({
+      nome: this.formBuilder.control('', [Validators.required]),
+      parentesco: this.formBuilder.control('', [Validators.required]),
+      rua: this.formBuilder.control('', [Validators.required]),
+      numero: this.formBuilder.control('', [Validators.required]),
+      bairro: this.formBuilder.control('', [Validators.required]),
+      cidade: this.formBuilder.control('', [Validators.required]),
+      estado: this.formBuilder.control('', [Validators.required]),
+      cep: this.formBuilder.control('', [Validators.required]),
+      complemento: this.formBuilder.control('', [Validators.required]),
+      referencia: this.formBuilder.control('', [Validators.required]),
+      telefone: this.formBuilder.control('', [Validators.required]),
+    })
+  }
+
+  familiarResidente(familiar: Familiar) {
+    console.log(familiar)
   }
 
 }
