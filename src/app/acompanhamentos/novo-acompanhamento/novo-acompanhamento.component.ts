03dcedde0d3a838809c2919237c7a14c31c4ba78
NathanBarsoti8 <nathanbarsoti@hotmail.com>
2018-12-04 14:16:13 -0200

    excluindo css

--- src/app/acompanhamentos/novo-acompanhamento/novo-acompanhamento.component.ts
+++ src/app/acompanhamentos/novo-acompanhamento/novo-acompanhamento.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'salv-novo-acompanhamento',
-  templateUrl: './novo-acompanhamento.component.html',
-  styleUrls: ['./novo-acompanhamento.component.css']
+  templateUrl: './novo-acompanhamento.component.html'
 })
 export class NovoAcompanhamentoComponent implements OnInit {
 
