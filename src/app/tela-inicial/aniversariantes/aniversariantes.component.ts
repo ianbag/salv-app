e161b81a4bda200c47c5540f12c66744d28054fd
NathanBarsoti8 <nathanbarsoti@hotmail.com>
2019-03-21 16:40:16 -0300

    animation -> aniversariantes and prova-de-vida

--- src/app/tela-inicial/aniversariantes/aniversariantes.component.ts
+++ src/app/tela-inicial/aniversariantes/aniversariantes.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'salv-aniversariantes',
   templateUrl: './aniversariantes.component.html',
-  styleUrls: ['./aniversariantes.component.css']
+  styleUrls: ['./aniversariantes.component.css'],
+  animations: [
+    trigger('aniversariantesAppeared', [
+      state('ready', style({opacity: 1})),
+      transition('void => ready', [
+        style({opacity: 0, transform: 'translate(-30px, -10px)'}),
+        animate('500ms 0s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class AniversariantesComponent implements OnInit {
+
+  aniversariantesState = 'ready'
 
   constructor() { }
 
