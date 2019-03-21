b13fa327f6ff07a6ae507ee0090715bee6e15766
NathanBarsoti8 <nathanbarsoti@hotmail.com>
2019-03-21 15:34:55 -0300

    animações funcionário (exceto editar e novo dependente, esperando as correçoes)

--- src/app/funcionarios/funcionario/novo-funcionario/novo-dependente/novo-dependente.component.ts
+++ src/app/funcionarios/funcionario/novo-funcionario/novo-dependente/novo-dependente.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'salv-novo-dependente',
   templateUrl: './novo-dependente.component.html',
-  styleUrls: ['./novo-dependente.component.css']
+  styleUrls: ['./novo-dependente.component.css'],
+  animations: [
+    trigger('novo-dependenteAppeared', [
+      state('ready', style({opacity: 1})),
+      transition('void => ready', [
+        style({opacity: 0, transform: 'translate(-30px, -10px)'}),
+        animate('500ms 0s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class NovoDependenteComponent implements OnInit {
+
+  novodependenteState = 'ready'
 
   constructor() { }
 
