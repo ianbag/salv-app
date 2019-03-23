b82000a0c04ce71da565385d5954144450a19a1f
Ian Rotondo Bagliotti <ian.bagliotti@gmail.com>
2019-03-22 22:09:28 -0300

    Alterado services de residentes

--- src/app/residentes/residentes.service.ts
+++ src/app/residentes/residentes.service.ts
@@ -2,7 +2,7 @@ import { SALV_API } from './../app.api';
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
-import { Residente } from './residente/residente.model';
+import { Residente, Pessoa } from './residente/residente.model';
 import { Familiar } from './residente/infos-familiar/familiar.model';
 import { Convenio } from './residente/infos-convenio/convenio.model';
 
@@ -29,5 +29,13 @@ export class ResidentesService {
 
     deleteResidente(id: string): Observable<any>{
         return this.http.delete<any>(`${SALV_API}/residente/${id}`)
+    }
+
+    createPessoa(pessoa: Pessoa): Observable<Pessoa>{
+        return this.http.post<Pessoa>(`${SALV_API}/pessoa`, pessoa)
+    }
+
+    createResidente(residente: Residente): Observable<Residente>{
+        return this.http.post<Residente>(`${SALV_API}/residente`, residente)
     }
 }
