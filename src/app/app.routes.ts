6797f83b29e75caa6cea5be408e869a3b87407fb
NathanBarsoti8 <nathanbarsoti@hotmail.com>
2019-03-26 11:23:36 -0300

    criação da rota de novo-convenio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { NovoConvenioComponent } from './convenios/novo-convenio/novo-convenio.component';
 import { NovoAcompanhamentoComponent } from './acompanhamentos/novo-acompanhamento/novo-acompanhamento.component';
 
 import { ConveniosComponent } from './convenios/convenios.component';
@@ -30,6 +31,7 @@ export const ROUTES: Routes = [
     { path: 'convenio-residente', component: ConvenioResidenteComponent, canActivate: [AuthGuard] },
     { path: 'convenios', component: ConveniosComponent, canActivate: [AuthGuard] },
     { path: 'convenio/:id', component: ConvenioComponent, canActivate: [AuthGuard] },
+    { path: 'novo-convenio', component: NovoConvenioComponent, canActivate: [AuthGuard] },
     { path: 'funcionarios', component: FuncionariosComponent, canActivate: [AuthGuard] },
     { path: 'funcionario/:id', component: FuncionarioComponent, canActivate: [AuthGuard] },
     { path: 'novo-funcionario', component: NovoFuncionarioComponent, canActivate: [AuthGuard] },
