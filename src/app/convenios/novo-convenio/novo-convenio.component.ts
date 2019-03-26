03e2870f0a3d227cf84cb3123b0de7fe33a94049
NathanBarsoti8 <nathanbarsoti@hotmail.com>
2019-03-26 11:29:14 -0300

    criação da animacao de novo-convenio

--- src/app/convenios/novo-convenio/novo-convenio.component.ts
+++ src/app/convenios/novo-convenio/novo-convenio.component.ts
@@ -1,10 +1,22 @@
+import { transition, style, trigger, state, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'salv-novo-convenio',
-  templateUrl: './novo-convenio.component.html'
+  templateUrl: './novo-convenio.component.html',
+  animations: [
+    trigger('novo-convenioAppeared', [
+      state('ready', style({opacity: 1})),
+      transition('void => ready', [
+        style({opacity: 0, transform: 'translate(-30px, -10px)'}),
+        animate('500ms 0s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class NovoConvenioComponent implements OnInit {
+
+  novoconvenioState = 'ready'
 
   constructor() { }
 
