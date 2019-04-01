d922e1f3697ca6cf18dc5fe33c8b6e219dda31aa
NathanBarsoti8 <nathanbarsoti@hotmail.com>
2019-04-01 10:30:55 -0300

    att conveio service

--- src/app/convenios/convenios.service.ts
+++ src/app/convenios/convenios.service.ts
@@ -2,7 +2,7 @@ import { SALV_API } from './../app.api';
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
-import { Convenio, Telefone } from "./convenio.model";
+import { Convenio, Telefone, Endereco, ConvenioQuery } from "./convenio.model";
 
 
 @Injectable()
@@ -14,16 +14,26 @@ export class ConveniosService {
         return this.http.get<Convenio[]>(`${SALV_API}/convenio`)
     }
 
-    conveniosById(id: number): Observable<Convenio>{
+    conveniosById(id: number): Observable<Convenio> {
         return this.http.get<Convenio>(`${SALV_API}/convenio/${id}`)
     }
 
-    telefoneById(id: string): Observable<Telefone[]>{
-        
+    telefoneById(id: string): Observable<Telefone[]> {
+        return this.http.get<Telefone[]>(`${SALV_API}/telefone_convenio/${id}`)
     }
 
-    deleteConvenio(id: string): Observable<any>{
+    enderecoById(id: string): Observable<Endereco[]> {
+        return this.http.get<Endereco[]>(`${SALV_API}/endereco_convenio/${id}`)
+    }
+
+    deleteConvenio(id: string): Observable<any> {
         return this.http.delete<any>(`${SALV_API}/convenio/${id}`)
     }
+
+    convenioQuery(id: string): Observable<ConvenioQuery[]> {
+        return this.http.get<ConvenioQuery[]>(`${SALV_API}/convenio-full/${id}`)
+    }
+
+    
 
 }
