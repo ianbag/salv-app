0190049c9e5cc4c4ca84754f011619f975ba870f
Ian Rotondo Bagliotti <ian.bagliotti@gmail.com>
2019-04-08 21:43:22 -0300

    Finalizado corrigido iteração telefone, criado navegação do botao voltar

--- src/app/residentes/novo-residente/familiar-residente/familiar-residente.component.ts
+++ src/app/residentes/novo-residente/familiar-residente/familiar-residente.component.ts
@@ -1,17 +1,20 @@
-import { Familiar } from './../../residente/infos-familiar/familiar.model';
-import { FormGroup, FormBuilder, Validators, FormArray, FormControl } from '@angular/forms';
+import { Familiar, Telefone } from './../../residente/infos-familiar/familiar.model';
+import { FormGroup, FormBuilder, Validators, FormArray, FormControl, AbstractControl } from '@angular/forms';
 import { Component, OnInit, Input } from '@angular/core';
 import { trigger, state, transition, style, animate } from '@angular/animations'
 import { ResidentesService } from '../../residentes.service';
+import { Endereco } from 'src/app/funcionarios/funcionario.model';
+import { Router } from '@angular/router';
+import { NotificationService } from 'src/app/shared/notification.service';
 
 @Component({
   selector: 'salv-familiar-residente',
   templateUrl: './familiar-residente.component.html',
   animations: [
     trigger('familiar-residenteAppeared', [
-      state('ready', style({opacity: 1})),
+      state('ready', style({ opacity: 1 })),
       transition('void => ready', [
-        style({opacity: 0, transform: 'translate(-30px, -10px)'}),
+        style({ opacity: 0, transform: 'translate(-30px, -10px)' }),
         animate('500ms 0s ease-in-out')
       ])
     ])
@@ -28,44 +31,107 @@ export class FamiliarResidenteComponent implements OnInit {
   familiarResidenteForm: FormGroup;
 
   familiar: Familiar
+  endereco: Endereco
+  telefone: Telefone[]
 
-  constructor(private formBuilder: FormBuilder, private residentesService: ResidentesService) { }
+  telefonesArray: FormArray
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private residentesService: ResidentesService,
+    private router: Router,
+    private notificationService: NotificationService
+  ) { }
+
+  markAllDirty(control: AbstractControl) {
+    if (control.hasOwnProperty('controls')) {
+      control.markAsDirty()
+      let ctrl = <any>control;
+      for (let inner in ctrl.controls)
+        this.markAllDirty(ctrl.controls[inner] as AbstractControl);
+    }
+    else 
+      (<FormControl>(control)).markAsDirty();
+  }
 
   ngOnInit() {
-    
+    this.endereco = this.residentesService.endereco
+    this.telefone = this.residentesService.telefones
+    this.familiar = this.residentesService.familiar
+
     this.familiarResidenteForm = this.formBuilder.group({
       NOME: this.formBuilder.control(null, [Validators.required]),
       SOBRENOME: this.formBuilder.control(null, [Validators.required]),
-      PARENTESCO: this.formBuilder.control(null, []),
+      PARENTESCO: this.formBuilder.control(null, [Validators.required]),
       ENDERECOS: this.formBuilder.group({
-        ENDERECO: this.formBuilder.control(null, []),
-        NUMERO: this.formBuilder.control(null, []),
-        BAIRRO: this.formBuilder.control(null, []),
-        CIDADE: this.formBuilder.control(null, []),
-        ESTADO: this.formBuilder.control(null, []),
-        CEP: this.formBuilder.control(null, []),
+        ENDERECO: this.formBuilder.control(null, [Validators.required]),
+        NUMERO: this.formBuilder.control(null, [Validators.required]),
+        BAIRRO: this.formBuilder.control(null, [Validators.required]),
+        CIDADE: this.formBuilder.control(null, [Validators.required]),
+        ESTADO: this.formBuilder.control(null, [Validators.required]),
+        CEP: this.formBuilder.control(null, [Validators.required]),
         COMPLEMENTO: this.formBuilder.control(null, []),
         REFERENCIA: this.formBuilder.control(null, []),
       }),
-      TELEFONES: this.formBuilder.array([])
+      TELEFONES: this.formBuilder.array([this.createTelefones()])
     })
+
+    if (this.endereco != undefined)
+      this.familiarResidenteForm.controls['ENDERECOS'].setValue(this.endereco)
+    //this.familiarResidenteForm.controls['TELEFONES'].setValue(this.telefone)
+    if (this.telefone != undefined)
+      this.telefone.forEach(element => {
+        this.addTelefones()
+        console.log("ELEMENTOS TELEFONE: ", element)
+      })
+    if (this.familiar != undefined)
+      this.familiarResidenteForm.patchValue(this.familiar)
   }
 
-  get telefones(){
+  createTelefones(): FormGroup {
+    return this.formBuilder.group({
+      DDD: this.formBuilder.control(null, [Validators.required, Validators.minLength(3)]),
+      NUMERO: this.formBuilder.control(null, [Validators.required, Validators.minLength(9)])
+    });
+  }
+
+  get telefones() {
     return this.familiarResidenteForm.get('TELEFONES') as FormArray
   }
 
-  addTelefones(){
-    this.telefones.push(new FormControl(''))
+  addTelefones() {
+    this.telefonesArray = this.familiarResidenteForm.get('TELEFONES') as FormArray;
+    this.telefonesArray.push(this.createTelefones());
+  }
+
+  removeTelefone(index) {
+    console.log('index;>: ', index);
+    if (this.telefonesArray.length != 1){
+      this.telefonesArray = this.familiarResidenteForm.get('TELEFONES') as FormArray;
+      this.telefonesArray.removeAt(index)
+    }
+  }
+
+  addDataServiceFamiliar(familiar: Familiar) {
+    this.residentesService.endereco = familiar.ENDERECOS
+    this.residentesService.telefones = this.telefones.value
+    this.residentesService.familiar = familiar
   }
 
   familiarResidente(familiar: Familiar) {
-    this.residentesService.endereco = familiar.ENDERECOS
-    this.residentesService.telefones = this.telefones.value
-    delete familiar.ENDERECOS
-    delete familiar.TELEFONE
-    this.residentesService.familiar = familiar
-    console.log("TELEFONES>: ", this.residentesService.telefones)
+    if (this.familiarResidenteForm.valid == true) {
+      this.addDataServiceFamiliar(familiar)
+      this.router.navigate(['/convenio-residente'])
+    }
+    else {
+      this.markAllDirty(this.familiarResidenteForm)
+      console.log(this.familiarResidenteForm.controls)
+      this.notificationService.notify(`Preencha os campos obrigatórios!`)
+    }
+  }
+
+  voltarResidente(familiar: Familiar) {
+    this.addDataServiceFamiliar(familiar)
   }
 
 }
