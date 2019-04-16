79aa128c5b22831f564c675c0e9dc14eef501fb7
Juliano <juliano-gema@hotmail.com>
2019-04-16 01:13:11 -0300

    screen loading implementado nas novas telas e bugs corrigidos

--- src/app/auth/forget-password/forget-password.component.ts
+++ src/app/auth/forget-password/forget-password.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { NotificationService } from './../../shared/notification.service'
 import { ForgetService } from './forget.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 import { Router } from '@angular/router';
 
 @Component({
@@ -13,18 +14,25 @@ export class ForgetPasswordComponent implements OnInit {
 
   forgetPasswordForm: FormGroup
 
-  constructor(private fb: FormBuilder, private fs: ForgetService, private ns: NotificationService, private router: Router) { }
+  constructor(private fb: FormBuilder, private fs: ForgetService, private ns: NotificationService, private router: Router, private spinner: NgxSpinnerService) { }
 
   ngOnInit() {
+    this.spinner.show()
     this.forgetPasswordForm = this.fb.group({
       EMAIL: this.fb.control(null, [Validators.required, Validators.email])
     })
-  }
 
+    setTimeout(() => {
+      this.spinner.hide();
+     }, 1000);
+  }
+ 
   sendMail(user: Usuario) {
+    this.spinner.show()
     this.fs.sendMail(user).subscribe((res: any) => {
       this.ns.notify(res.message)
       this.router.navigate(['/login'])
+      
     })
   }
 
