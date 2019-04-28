f7aed0b3e08d6c135c0b25e08b2a2be45e355d30
Ian Rotondo Bagliotti <ian.bagliotti@gmail.com>
2019-04-28 17:04:11 -0300

    criado model de beneficios

---
+++ src/app/residentes/residente/infos-beneficios/beneficio.model.ts
@@ -0,0 +1,9 @@
+export interface Beneficio {
+    NOME_BENEFICIO: string,
+    CODIGO_RESIDENTE: number,
+    BANCO_BENEFICIO: string,
+    AGENCIA_BENEFICIO: string,
+    CONTA_BENEFICIO: string,
+    VALOR_BENEFICIO: number,
+    PROVA_VIDA_BENEFICIO: string
+}
