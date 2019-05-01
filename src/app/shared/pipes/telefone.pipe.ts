5b168610ede52bc6f60764a53f8ba15f7bcef8ed
hppod <hppodgornik@gmail.com>
2019-05-01 11:40:29 -0300

    Melhorando a implementação do pipe telefone para que ele exiba o nono digito caso seja celular

--- src/app/shared/pipes/telefone.pipe.ts
+++ src/app/shared/pipes/telefone.pipe.ts
@@ -8,14 +8,20 @@ export class TelefonePipe implements PipeTransform {
   transform(value: any): any {
     let telefone = value
 
-    if (telefone) {
+    if (telefone.length == '8') {
       return telefonePoints(telefone)
+    } else if (telefone.length == '9') {
+      return celularPoints(telefone)
     } else {
       return 'Não declarado'
     }
 
     function telefonePoints(value?: string): string {
-      return `${value.substr(0, 4)}-${value.substr(4, 5)}`
+      return `${value.substr(0, 4)}-${value.substr(4, 4)}`
+    }
+
+    function celularPoints(value?: string): string {
+      return `${value.substr(0, 5)}-${value.substr(5, 4)}`
     }
   }
 
