bfc1c9e04234c042e3256d6324a4aff50abe2347
hppod <hppodgornik@gmail.com>
2019-05-07 00:39:18 -0300

    environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiUrl: 'https://drvida-api.herokuapp.com',
+  apiUrl: 'https://www.drvida-api.herokuapp.com',
   production: true
 };
 
