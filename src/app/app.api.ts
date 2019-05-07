bc834c15e0877d276e34a76f357746072ff4df7f
hppod <hppodgornik@gmail.com>
2019-05-06 23:39:58 -0300

    Alterando environment

--- src/app/app.api.ts
+++ src/app/app.api.ts
@@ -1,1 +1,6 @@
-export const SALV_API = 'http://localhost:3000'
+import { environment } from './../environments/environment.prod'
+
+export const SALV_API = environment
+
+
+// export const SALV_API = 'http://localhost:3000'
