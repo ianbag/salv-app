fee6fb6b3b1666258c55f84a017d613b4259be3a
hppod <hppodgornik@gmail.com>
2019-05-07 01:29:40 -0300

    url

--- src/app/app.api.ts
+++ src/app/app.api.ts
@@ -1,6 +1,6 @@
 import { environment } from './../environments/environment'
 
-export const SALV_API = environment
+export const SALV_API = environment.apiUrl
 
 
 // export const SALV_API = 'http://localhost:3000'
