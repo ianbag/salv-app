463efccfd659d7fe9848f4bd34d950e0936a2ced
hppod <hppodgornik@gmail.com>
2019-05-11 15:42:48 -0300

    Comments

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,19 @@
+//Modules
 import { NgModule } from '@angular/core'
 
 //Components
 import { InputComponent } from './input/input.component'
 import { SnackbarComponent } from './snackbar/snackbar.component'
 
+//Pipes
+
+
 @NgModule({
     declarations: [
+        //Components
         InputComponent,
-        SnackbarComponent
+        SnackbarComponent,
+        //Pipes
     ]
 })
 export class SharedModule { }
