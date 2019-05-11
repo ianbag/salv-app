f871d7e77b048eef32d6b6d250e036ac8c5731ac
hppod <hppodgornik@gmail.com>
2019-05-11 17:57:24 -0300

    Delete files and import components

--- src/app/acompanhamentos/acompanhamento.module.ts
+++ src/app/acompanhamentos/acompanhamento.module.ts
@@ -3,12 +3,20 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from './../shared/shared.module';
 import { RouterModule, Routes } from '@angular/router';
 
+//Components
+import { AcompanhamentosComponent } from './acompanhamentos.component';
+import { AcompanhamentoComponent } from './acompanhamento/acompanhamento.component';
+import { NovoAcompanhamentoComponent } from './novo-acompanhamento/novo-acompanhamento.component';
+
 @NgModule({
     declarations: [
-
+        AcompanhamentosComponent,
+        AcompanhamentoComponent,
+        NovoAcompanhamentoComponent
     ],
     imports: [
-
+        SharedModule,
+        RouterModule.forChild(ROUTES)
     ]
 })
 export class AcompanhamentoModule { }
