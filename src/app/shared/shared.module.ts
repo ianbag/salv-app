2b8ee20d2a6ef0f32b4ccc84426d45a485d508e5
hppod <hppodgornik@gmail.com>
2019-05-11 15:50:16 -0300

    Import general pipes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,7 +6,20 @@ import { InputComponent } from './input/input.component'
 import { SnackbarComponent } from './snackbar/snackbar.component'
 
 //Pipes
-
+import { ApelidoPipe } from './pipes/apelido.pipe'
+import { CartaoSamsPipe } from './pipes/cartao-sams.pipe';
+import { CartaoSUSPipe } from './pipes/cartaoSUS.pipe';
+import { CepPipe } from './pipes/cep.pipe';
+import { CpfPipe } from './pipes/cpf.pipe';
+import { DataPipe } from './pipes/data.pipe';
+import { EscolaridadePipe } from './pipes/escolaridade.pipe';
+import { EstadoCivilPipe } from './pipes/estado-civil.pipe';
+import { SearchPipe } from './pipes/filtroData.pipe';
+import { ProfissaoPipe } from './pipes/profissao.pipe';
+import { ReligiaoPipe } from './pipes/religiao.pipe';
+import { RgPipe } from './pipes/rg.pipe';
+import { SexoPipe } from './pipes/sexo.pipe';
+import { TelefonePipe } from './pipes/telefone.pipe';
 
 @NgModule({
     declarations: [
@@ -14,6 +27,20 @@ import { SnackbarComponent } from './snackbar/snackbar.component'
         InputComponent,
         SnackbarComponent,
         //Pipes
+        ApelidoPipe,
+        CartaoSamsPipe,
+        CartaoSUSPipe,
+        CepPipe,
+        CpfPipe,
+        DataPipe,
+        EscolaridadePipe,
+        EstadoCivilPipe,
+        SearchPipe,
+        ProfissaoPipe,
+        ReligiaoPipe,
+        RgPipe,
+        SexoPipe,
+        TelefonePipe
     ]
 })
 export class SharedModule { }
