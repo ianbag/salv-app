cac3c7004a1a1d726cf369dcdac7421927c6da90
hppod <hppodgornik@gmail.com>
2019-05-11 16:05:51 -0300

    Fix erros in shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 //Modules
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 //Components
 import { InputComponent } from './input/input.component';
@@ -83,7 +83,7 @@ import { ValorInssPipe } from './pipes/inss/valor-inss.pipe';
         //Modules
         CommonModule,
         FormsModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
         //Components
         InputComponent,
         SnackbarComponent,
