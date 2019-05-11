d7c110dbef28c476f6af864c21a60cb278257a13
hppod <hppodgornik@gmail.com>
2019-05-11 17:38:05 -0300

    Import components and declare

--- src/app/funcionarios/funcionario.module.ts
+++ src/app/funcionarios/funcionario.module.ts
@@ -3,12 +3,24 @@ import { NgModule } from '@angular/core'
 import { SharedModule } from './../shared/shared.module'
 import { RouterModule, Routes } from '@angular/router'
 
+//Components
+import { FuncionariosComponent } from './funcionarios.component'
+import { FuncionarioComponent } from './funcionario/funcionario.component'
+import { InfosFuncionarioComponent } from './funcionario/infos-funcionario/infos-funcionario.component'
+import { InfosDependenteComponent } from './funcionario/infos-dependente/infos-dependente.component'
+import { NovoFuncionarioComponent } from './novo-funcionario/novo-funcionario.component'
+
 @NgModule({
     declarations: [
-
+        FuncionariosComponent,
+        FuncionarioComponent,
+        InfosFuncionarioComponent,
+        InfosDependenteComponent,
+        NovoFuncionarioComponent
     ],
     imports: [
-
+        SharedModule,
+        RouterModule.forChild(ROUTES)
     ]
 })
 export class FuncionarioModule { }
