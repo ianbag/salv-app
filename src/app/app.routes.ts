d7db380f1b0291d019e9daeb00c15996e3731d8e
hppod <hppodgornik@gmail.com>
2019-05-11 18:05:59 -0300

    Delete routes acompanhamento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,16 @@
 import { Routes } from "@angular/router";
 
 import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
-import { NovoAcompanhamentoComponent } from './acompanhamentos/novo-acompanhamento/novo-acompanhamento.component';
+import { TelaInicialComponent } from './tela-inicial/tela-inicial.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './auth/login/login.component'
+import { ForgetPasswordComponent } from './auth/forget-password/forget-password.component';
 
 import { ResidentesComponent } from "./residentes/residentes.component";
 import { ResidenteComponent } from "./residentes/residente/residente.component";
-import { TelaInicialComponent } from './tela-inicial/tela-inicial.component';
-import { NotFoundComponent } from './not-found/not-found.component';
 import { NovoResidenteComponent } from './residentes/novo-residente/novo-residente.component';
-import { AcompanhamentosComponent } from './acompanhamentos/acompanhamentos.component';
-import { AcompanhamentoComponent } from './acompanhamentos/acompanhamento/acompanhamento.component';
 import { FamiliarResidenteComponent } from './residentes/novo-residente/familiar-residente/familiar-residente.component';
 import { ConvenioResidenteComponent } from './residentes/novo-residente/convenio-residente/convenio-residente.component';
-import { LoginComponent } from './auth/login/login.component'
-import { ForgetPasswordComponent } from './auth/forget-password/forget-password.component';
 
 import { AuthGuardService as AuthGuard } from './auth/auth-guard.service'
 
@@ -27,16 +24,13 @@ export const ROUTES: Routes = [
 
     { path: 'convenios', loadChildren: './convenios/convenio.module#ConvenioModule', canActivate: [AuthGuard] },
     { path: 'funcionarios', loadChildren: './funcionarios/funcionario.module#FuncionarioModule', canActivate: [AuthGuard] },
+    { path: 'acompanhamentos', loadChildren: './acompanhamentos/acompanhamento.module#AcompanhamentoModule', canActivate: [AuthGuard] },
 
     { path: 'residentes', component: ResidentesComponent, canActivate: [AuthGuard] },
     { path: 'residente/:id', component: ResidenteComponent, canActivate: [AuthGuard] },
     { path: 'novo-residente', component: NovoResidenteComponent, canActivate: [AuthGuard] },
     { path: 'familiar-residente', component: FamiliarResidenteComponent, canActivate: [AuthGuard] },
     { path: 'convenio-residente', component: ConvenioResidenteComponent, canActivate: [AuthGuard] },
-
-    { path: 'acompanhamentos', component: AcompanhamentosComponent, canActivate: [AuthGuard] },
-    { path: 'acompanhamento/:id', component: AcompanhamentoComponent, canActivate: [AuthGuard] },
-    { path: 'novo-acompanhamento', component: NovoAcompanhamentoComponent, canActivate: [AuthGuard] },
 
     { path: '**', component: NotFoundComponent }
 ]
