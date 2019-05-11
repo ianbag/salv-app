69fb90bdcd974ac451044818bbf70bfc530abdb7
hppod <hppodgornik@gmail.com>
2019-05-11 16:20:44 -0300

    Routes on convenio module

--- src/app/convenios/convenio.module.ts
+++ src/app/convenios/convenio.module.ts
@@ -1,11 +1,19 @@
 //Modules
 import { NgModule } from '@angular/core';
 import { SharedModule } from './../shared/shared.module';
+import { RouterModule, Routes } from '@angular/router'
 
 //Components
 import { ConveniosComponent } from './convenios.component';
 import { ConvenioComponent } from './convenio/convenio.component';
 import { NovoConvenioComponent } from './novo-convenio/novo-convenio.component';
+
+//Routes
+const ROUTES: Routes = [
+    { path: '', component: ConveniosComponent },
+    { path: 'convenio/:id', component: ConvenioComponent },
+    { path: 'novo-convenio', component: NovoConvenioComponent }
+]
 
 @NgModule({
     declarations: [
@@ -14,7 +22,8 @@ import { NovoConvenioComponent } from './novo-convenio/novo-convenio.component';
         NovoConvenioComponent
     ],
     imports: [
-        SharedModule
+        SharedModule,
+        RouterModule.forChild(ROUTES)
     ]
 })
 export class ConvenioModule { }
