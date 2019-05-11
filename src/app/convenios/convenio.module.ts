f2e3b6efb64f62554c0eca64d510d3591b5bcaf7
hppod <hppodgornik@gmail.com>
2019-05-11 16:17:17 -0300

    Convenio module

---
+++ src/app/convenios/convenio.module.ts
@@ -0,0 +1,18 @@
+//Modules
+import { NgModule } from '@angular/core';
+import { SharedModule } from './../shared/shared.module';
+
+//Components
+import { ConveniosComponent } from './convenios.component';
+import { ConvenioComponent } from './convenio/convenio.component';
+import { NovoConvenioComponent } from './novo-convenio/novo-convenio.component';
+
+@NgModule({
+    declarations: [
+
+    ],
+    imports: [
+
+    ]
+})
+export class ConvenioModule { }
