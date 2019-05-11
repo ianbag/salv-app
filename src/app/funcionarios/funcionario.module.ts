1097241936e7a350e5dcf3f2c55add6cd8af2865
hppod <hppodgornik@gmail.com>
2019-05-11 17:34:51 -0300

    Module funcionario

---
+++ src/app/funcionarios/funcionario.module.ts
@@ -0,0 +1,14 @@
+//Modules
+import { NgModule } from '@angular/core'
+import { SharedModule } from './../shared/shared.module'
+import { RouterModule, Routes } from '@angular/router'
+
+@NgModule({
+    declarations: [
+
+    ],
+    imports: [
+
+    ]
+})
+export class FuncionarioModule { }
