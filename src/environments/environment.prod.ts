c6ac479c9cbd0f34f1be462c99cb4a03490ef617
Ian Rotondo Bagliotti <ian.bagliotti@gmail.com>
2019-05-14 12:04:12 -0300

    corrigido validacao de telefone familiar

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://drvida-api.herokuapp.com'
+  // apiUrl: 'https://drvida-api.herokuapp.com'
+    apiUrl: 'http://localhost:3000'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: true,
-  apiUrl: 'https://drvida-api.herokuapp.com'
+  // apiUrl: 'https://drvida-api.herokuapp.com'
+    apiUrl: 'http://localhost:3000'
 };
 
 /*
