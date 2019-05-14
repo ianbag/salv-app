b1900808ee9b37267cb2c1f6a5ce3b19611b1fa1
Ian Rotondo Bagliotti <ian.bagliotti@gmail.com>
2019-05-14 12:06:12 -0300

    retornando URL api environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://drvida-api.herokuapp.com'
-    apiUrl: 'http://localhost:3000'
+  apiUrl: 'https://drvida-api.herokuapp.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: true,
-  // apiUrl: 'https://drvida-api.herokuapp.com'
-    apiUrl: 'http://localhost:3000'
+  apiUrl: 'https://drvida-api.herokuapp.com'
 };
 
 /*
