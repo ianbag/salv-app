3d498864b5c128bdeb16c8c029848e37b485219d
hppod <hppodgornik@gmail.com>
2019-05-16 23:22:55 -0300

    Fix pipes

--- src/app/shared/pipes/religiao.pipe.ts
+++ src/app/shared/pipes/religiao.pipe.ts
@@ -8,17 +8,17 @@ export class ReligiaoPipe implements PipeTransform {
   transform(value: any): any {
     let religiao
 
-    if (value == 'cat' || 'CAT') {
+    if (value == 'CAT') {
       religiao = 'Católico'
-    } else if (value == 'evg' || 'EVG') {
+    } else if (value == 'EVG') {
       religiao = 'Evangélico'
-    } else if (value == 'esp' || 'ESP') {
+    } else if (value == 'ESP') {
       value = 'Espirita'
-    } else if (value == 'ubc' || 'UBC') {
+    } else if (value == 'UBC') {
       value = 'Umbanda e Candomblé'
-    } else if (value == 'srg' || 'SRG') {
+    } else if (value == 'SRG') {
       value = 'Sem religião'
-    } else if (value == 'out' || 'OUT') {
+    } else if (value == 'OUT') {
       value = 'Outras'
     } else {
       religiao = 'Não especificada'
