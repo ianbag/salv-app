59d4fe3ec0d5241de0982ca6766a32339942760c
hppod <hppodgornik@gmail.com>
2019-05-19 21:47:11 -0300

    Import login service and show toastr on first access

--- src/app/tela-inicial/tela-inicial.component.ts
+++ src/app/tela-inicial/tela-inicial.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NotificationService } from '../shared/notification.service';
 import { ToastrService } from 'ngx-toastr';
+import { LoginService } from './../auth/login/login.service'
 
 @Component({
   selector: 'salv-tela-inicial',
@@ -9,10 +10,10 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class TelaInicialComponent implements OnInit {
 
-  constructor(private ns: NotificationService, private toastr: ToastrService) { }
+  constructor(private ns: NotificationService, private toastr: ToastrService, private ls: LoginService) { }
 
   ngOnInit() {
-    if ('TESTE') {
+    if (this.ls.primeiro_acesso == 1) {
       this.toastr.info('Primeiro acesso', 'É seu primeiro acesso e o toastr está funcionando corretamente')
     }
   }
